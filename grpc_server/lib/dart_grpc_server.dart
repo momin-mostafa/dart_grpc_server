4be523dcf7632726251f57c724e4709d5aef741f
momin-mostafa <mominmostafa.g@gmail.com>
2022-06-15 17:30:07 +0600

    init

---
+++ grpc_server/lib/dart_grpc_server.dart
@@ -0,0 +1,8 @@
+export 'src/db/data.dart';
+export 'src/generated/groceries.pb.dart';
+export 'src/generated/groceries.pbgrpc.dart';
+export 'src/helper/methods.dart';
+export 'src/db/categories/categories_implemenation.dart';
+export 'src/db/categories/categories_interface.dart';
+export 'src/db/items/items_implementation.dart';
+export 'src/db/items/items_interface.dart';
---
+++ grpc_server/lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:get_server/get_server.dart';
+
+void main() {
+  runApp(GetServer());
+}
